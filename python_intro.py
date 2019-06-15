473b7af68337d807ed3cfbd5cf55442f1773fe68
Oliver <oliverjoris@gmail.com>
2019-06-15 23:31:12 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,20 @@
+#if 5>2:
+#    print('5 is indeed greater than 2')
+#else:
+#    print('5 is not greater than 2')
+
+def hi(name):
+#name = 'Olly'
+#    if name == 'Olly':
+        print('hey ' + name + '!')
+#    elif name == 'Bob':
+#        print('hey Bob!')
+#    else:
+#        print('hey anonymous!')
+
+#hi('Bob')
+
+girls=['Rachel', 'Monica', 'Phoebe', 'Ola', 'Olly']
+
+for i in range(1,3):
+    print(i)
